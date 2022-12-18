2399d43d314c245952d5a7c5d77d8a7bf225f2eb
Kostayne <fuckogg@yandex.ru>
2022-12-18 16:16:33 +0300

    added tests to img_tag_info_collector

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,4 +3,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  
+  moduleNameMapper: {
+    '(.*)\\.js$': '$1',
+  },
 };
--- src/modules/img_tag_info_collector/img_tag_info_collector.test.ts
+++ src/modules/img_tag_info_collector/img_tag_info_collector.test.ts
@@ -1,15 +1,33 @@
+import 'reflect-metadata';
 import { Container } from 'typedi';
-import { defaultCfg } from '../../shared/default_cfg.js';
+import { IResultConfig } from '../../types/cfg.type.js';
+import { IDomImgInfo } from '../../types/dom_img_info.type.js';
+import { ImgHelperConfigLoader } from '../../utils/config/config_loader.js';
+import { getResultCfg } from '../../utils/config/get_result_cfg.js';
 import { ImgTagInfoCollector } from './img_tag_info_collector.module.js';
 
 describe('img_tag_info_collector module', () => {
+    let cfg: IResultConfig = null;
+    let collector: ImgTagInfoCollector = null;
+    let imgTagsInfo: IDomImgInfo[] = [];
+
+    beforeAll(async () => {
+        const fileCfg = await ImgHelperConfigLoader.loadFromRoot();
+        cfg = getResultCfg(fileCfg);
+
+        Container.set('cfg', cfg);
+
+        collector = Container.get(ImgTagInfoCollector);
+        imgTagsInfo = await collector.collectImgTagsInfoFromBrowser();
+    });
+
+    it('distinguishes different img tags with same src', () => {
+        const diffImgTagsWithSameSrc = imgTagsInfo.filter(img => img.src.endsWith('hunter.jpg'));
+        expect(diffImgTagsWithSameSrc.length).toBe(2);
+    });
+
     it('sees conditionally rendered imgs', async () => {
-        Container.set('cfg', defaultCfg);
-        const collector = Container.get(ImgTagInfoCollector);
-
-        const imgTagsInfo = await collector.collectImgTagsInfoFromBrowser();
-
-        // todo
-        expect(imgTagsInfo).not.toBeNull();
+        const img = imgTagsInfo.find(img => img.src.endsWith('bebop.jpg'));
+        expect(img).toBeTruthy();
     });
 });
